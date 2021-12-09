53257c50082cabd6a2e5ec08271274a690e2cc95
OptimBro <optimbro@outlook.com>
2021-12-09 12:36:08 +0530

    pagination stuff

---
+++ pages/products/index.js
@@ -0,0 +1,13 @@
+import Pagination from '../../components/Pagination';
+import Products from '../../components/Products';
+
+export default function ProductsPage({ query }) {
+  const page = parseInt(query.page);
+  return (
+    <div>
+      <Pagination page={page || 1} />
+      <Products page={page || 1} />
+      <Pagination page={page || 1} />
+    </div>
+  );
+}
